5ab195daeeb50a0113e8b87a8140cf5f786a04b7
acydcross <acydcross@hotmail.com>
2020-10-30 23:19:07 -0600

    Created a function that takes 2 arguments on each modules

--- addition.py
+++ addition.py
@@ -1,1 +1,3 @@
-print("plus")
+number1 = int(input('Enter your first number: '))
+number2 = int(input('Enter your second number: '))
+print("Your result: ", (number1 + number2))
--- app.py
+++ app.py
@@ -1,23 +1,18 @@
-import addition
-import subtraction
-import multiplication
-import division
+
 
 print("Welcome to the simple calculator, please select from the following options:")
 print("1. Addition")
 print("2. Subtraction")
 print("3. Multiplication")
-print("4. Division")
+print("4. division")
 
 userSelect = input("Please enter your selection: ")
 if userSelect in ('1', '2', '3', '4' ):
-    number1 = int(input("Please enter your first number: "))
-    number2 = int(input("Please enter your second number: "))
     if userSelect == '1':
-        print("Your result: ", (number1 + number2))
-    elif userSelect == '2':
-        print("Your result: ", (number1 - number2))
-    elif userSelect == '3':
-        print("Your result: ", (number1 * number2))
-    elif userSelect == '4':
-        print("Your result: ", (number1 / number2))
+        import addition
+    if userSelect == '2':
+        import subtraction
+    if userSelect == '3':
+        import multiplication
+    if userSelect == '4':
+        import division
